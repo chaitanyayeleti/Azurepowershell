d1394ac9bff2800d6a1f7128783c2bc5774700ae
chaitanyayeleti <87142103+chaitanyayeleti@users.noreply.github.com>
2021-07-08 22:02:29 +0530

    Add files via upload
    Above script will use to copy snapshot from one Tenant to another Tenant on Azure

---
+++ snapshotmigration.ps1
@@ -0,0 +1,25 @@
+﻿#login with source account
+Connect-AzAccount
+
+# subscrpitionID of source account
+$subID= ""
+#snapshoot resource group
+$RGName= ""
+#Snapshoot name
+$snapshotName= ""
+$SaasExpiryduration= 3600
+#destination Storage account name
+$storageaccountname= ""
+#destination contianer  name
+$storageaccountcontainername= ""
+##destination Storage account saas key
+$storageaccountkey= ""
+#Provide filename on destation -----.vhd
+$destinationVHDfilename= "Amtryu.vhd"
+
+Select-AzSubscription -Subscription $subID
+
+$sas = Grant-AzSnapshotAccess -ResourceGroupName $RGName -SnapshotName $snapshotName -DurationInSecond $SaasExpiryduration -Access Read
+$destinationContext = New-AzStorageContext -StorageAccountName $storageaccountname -StorageAccountKey $storageaccountkey
+
+Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageaccountcontainername -DestContext $destinationContext -DestBlob $destinationVHDfilename
